5e386c650baaf27f338d1e2d0a04bd923fe14df1
geofrey <devongeoffreymaina@gmail.com>
2024-07-17 10:37:59 +0300

    Fixed the Project Section Image Carousel

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,6 +11,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 // icons
@@ -18,35 +19,45 @@ import { GiClick } from "react-icons/gi";
 
 export default function Projects() {
   return (
-    <div className=" p-2 md:p-20 bg-black">
-      <div className="web-projects">
-        <div className="text-center mb-8">
-          <h3 className="text-4xl md:text-6xl font-bold text-white ">
-            WEB PROJECTS
-          </h3>
-          <span className="text-white text-sm font-extralight italic">
-            Scroll to view
-          </span>
-        </div>
-        <Carousel className="">
-          <CarouselContent className="">
-            {PROJECTS.map((proj) => (
-              <CarouselItem key={proj.id} className="basis-5/6 md:basis-3/6">
-                <Image
-                  src={proj.photo}
-                  width={1200}
-                  height={1200}
-                  alt={proj.name}
-                  className="w-full md:h-fit border-8 border-[#2A9FDE]"
-                />
-                <div className="mt-3 flex justify-center items-center">
-                  <Link
-                    href={proj.url}
-                    className="bg-[#ffffff] rounded-xl p-2 text-main font-bold"
-                  >
-                    {proj.name}
-                  </Link>
-                  <GiClick size={40} className="text-main ml-4" />
+    <div className="p-2 md:p-4 bg-image">
+      <div className="text-center mb-8">
+        <h3 className="text-4xl md:text-6xl font-bold text-white">
+          WEB PROJECTS
+        </h3>
+        <span className="text-gray-200 text-sm font-extralight italic">
+          Scroll to view
+        </span>
+      </div>
+      <div className="flex justify-center items-center">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          orientation="horizontal"
+          className="w-full max-w-7xl"
+        >
+          <CarouselContent className="-mt-1 h-[300px] md:h-[400px]">
+            {PROJECTS.map((proj, id) => (
+              <CarouselItem key={id} className="pt-1 md:basis-1/2">
+                <div className="p-1">
+                  <Card className="">
+                    <CardContent className="flex items-center justify-center p-6">
+                      <Image
+                        src={proj.photo}
+                        width={1200}
+                        height={1200}
+                        alt={proj.name}
+                        className="w-full md:h-fit border-8 rounded-xl border-[#29afde] shadow-lg shadow-white"
+                      ></Image>
+                    </CardContent>
+                    <CardFooter className="flex justify-center items-center">
+                      <Link href={proj.url}>
+                        <Button className="w-full bg-white hover:bg-[#2a9fde] text-[#2a9fde] hover:text-white">
+                          {proj.name}
+                        </Button>
+                      </Link>
+                    </CardFooter>
+                  </Card>
                 </div>
               </CarouselItem>
             ))}
