f652c3ee0143b207d60fe751078d2bd7141fdcaa
geofrey <devongeoffreymaina@gmail.com>
2025-06-27 18:45:46 +0300

    fixed the carousel buttons

--- components/Design.tsx
+++ components/Design.tsx
@@ -39,7 +39,7 @@ export default function Design() {
               {DESIGN.map((proj) => (
                 <CarouselItem
                   key={proj.id}
-                  className="pl-4 md:pl-6 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4"
+                  className="pl-4 md:pl-6 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/3"
                 >
                   <Card className="group overflow-hidden border-0 shadow-lg hover:shadow-xl transition-all duration-300 ease-out hover:-translate-y-1">
                     <CardContent className="p-0 relative aspect-square">
@@ -59,8 +59,8 @@ export default function Design() {
 
             {/* Custom positioned navigation */}
             <div className="hidden md:block">
-              <CarouselPrevious className="absolute -left-12 top-1/2 -translate-y-1/2 bg-white/90 border-2 hover:bg-white hover:scale-110 transition-all duration-200" />
-              <CarouselNext className="absolute -right-12 top-1/2 -translate-y-1/2 bg-white/90 border-2 hover:bg-white hover:scale-110 transition-all duration-200" />
+              <CarouselPrevious className="absolute -left-2 top-1/2 -translate-y-1/2 bg-white/90 border-2 hover:bg-white hover:scale-110 transition-all duration-200" />
+              <CarouselNext className="absolute -right-2 top-1/2 -translate-y-1/2 bg-white/90 border-2 hover:bg-white hover:scale-110 transition-all duration-200" />
             </div>
 
             {/* Mobile navigation */}
